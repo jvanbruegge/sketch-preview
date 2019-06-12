9098911610c3ad2bc3ca16eaf084325b573cd690
Jan van Brügge <supermanitu@gmail.com>
2019-06-12 11:16:03 +0200

    Offload unmerged PRs to different file for diffing

---
+++ src/diffViewer.ts
@@ -0,0 +1,3 @@
+export function diffViewer(): void {
+    console.log('this is not a merged PR');
+}
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,38 @@
 import { ImageData } from './background';
 import { fromByteArray } from 'base64-js';
 
+import { diffViewer } from './diffViewer';
+
 function checkForSketchFiles(): void {
-    const prFiles = Array.from(
-        document.querySelectorAll('.file-header[data-file-type=".sketch"]')
-    )
-        .map(
-            el =>
-                el!.querySelector<HTMLAnchorElement>(
-                    'details-menu a[data-ga-click*="View file"]'
-                )!.href + "?raw=true"
-        );
+    if(window.location.href.indexOf("/pull/") !== -1 && !document.querySelector(".State.State--purple")) {
+        diffViewer();
+    } else {
+        const prFiles = Array.from(
+            document.querySelectorAll('.file-header[data-file-type=".sketch"]')
+        )
+            .map(
+                el =>
+                    el!.querySelector<HTMLAnchorElement>(
+                        'details-menu a[data-ga-click*="View file"]'
+                    )!.href + "?raw=true"
+            );
 
 
-    const files = Array.from(document.querySelectorAll('.repository-content a#raw-url[href$=".sketch"]'))
-        .map(el => (el as HTMLAnchorElement).href);
+        const files = Array.from(document.querySelectorAll('.repository-content a#raw-url[href$=".sketch"]'))
+            .map(el => (el as HTMLAnchorElement).href);
 
-    prFiles.concat(files).forEach(url => {
-            const div = getPreviewDiv();
-            const img = div.firstChild as Element;
+        prFiles.concat(files).forEach(url => {
+                const div = getPreviewDiv();
+                const img = div.firstChild as Element;
 
-            if(!img || img.tagName !== "IMG")
-            {
-                console.log(`fetching ${url}`);
-                div.innerText = "Loading preview...";
-                chrome.runtime.sendMessage(url, displayZip);
-            }
-        });
+                if(!img || img.tagName !== "IMG")
+                {
+                    console.log(`fetching ${url}`);
+                    div.innerText = "Loading preview...";
+                    chrome.runtime.sendMessage(url, displayZip);
+                }
+            });
+    }
 }
 
 function displayZip(previewData: ImageData): void {
